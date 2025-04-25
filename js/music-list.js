dffaf0efb88fe32b9d331765e53518456c800c65
GabriiellR16 <gabriel.dias@minastc.com.br>
2025-04-25 15:05:17 -0300

    Feat: Adição de libs e outras funcionalidades

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "Pra você guardei o amor",
+        artist: "Ana Cañas",
+        src: "guardei_amor"
+    },
+    {
+        name: "Seu Astral",
+        artist: "Jorge e Matheus",
+        // img: "adventuretime",
+        src: "seu_astral"
+    },
+    {
+        name: "Duas Metades",
+        artist: "Jorge e Matheus",
+        // img: "adventuretime",
+        src: "duas_metades"
+    },
+    {
+        name: "Aliança",
+        artist: "Tribalistas",
+        // img: "adventuretime",
+        src: "alianca"
+    },
+    {
+        name: "Vilarejo",
+        artist: "Tribalistas",
+        // img: "adventuretime",
+        src: "vilarejo"
+    },
+    {
+        name: "Chuva de Arroz",
+        artist: "Luan Santana",
+        // img: "adventuretime",
+        src: "chuva_arroz"
+    },
+
+]
